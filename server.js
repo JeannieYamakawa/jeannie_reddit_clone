956f051dc898a7ec06510106b9fac2bc4b2f70d5
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-09-22 10:31:08 -0500

    more setup

---
+++ server.js
@@ -0,0 +1,41 @@
+var port = process.env.NODE_ENV || 3000;
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var knex = require('knex');
+
+// app.set("view engine", "ejs");
+// app.use(express.static(__dirname + "/public"));
+// app.use(morgan("tiny"));
+// app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+
+app.get('/', function(req, res) {
+  res.send('send is working')
+});
+
+
+
+app.get('/users', function(req, res) {
+  res.send('send is working')
+});
+
+
+
+app.get('/users/new', function(req, res) {
+
+  res.send('sign up')
+});
+
+
+
+
+app.listen(port, function(){
+    console.log("port is listening!")
+})
+
+module.exports = app;
---
+++ test/mypage_spec.js
@@ -0,0 +1,61 @@
+var app = require('../server');
+var request = require('supertest');
+var mocha = require('mocha');
+var expect = require('chai').expect;
+
+
+
+
+describe('GET /', function() {
+  it('should display our proper text', function(done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err,res){
+          expect(res.text).to.equal('send is working')
+          done()
+      })
+  });
+});
+
+
+describe('GET /users', function() {
+  it('should display all users', function(done) {
+    request(app)
+      .get('/users/new')
+      .end(function(err, res) {
+        if(err) return done(err);
+        expect(res.text).to.equal('send is working')
+        done()
+      })
+      // .expect(200, done);
+  });
+});
+
+
+  describe('GET /users/new', function() {
+    it('should take us to new users page', function(done) {
+      request(app)
+        .get('/users/new')
+        .end(function(err,res) {
+          if(err) return done(err);
+          expect(res.text).to.include('sign up')
+          done()
+        })
+    });
+
+    it('should add a new user', function(done) {
+      request(app)
+        .post('/users')
+        .send({id:1, name: bob})
+        .end(function(err,res) {
+            request(app).get('users')
+          expect(res.text).to.include('bob')
+          done()
+        })
+    });
+
+  });
+
+
+// request(app)
