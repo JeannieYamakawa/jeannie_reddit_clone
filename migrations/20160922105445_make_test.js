accef469a00dc9c6c63a5a95839c68582b20fdcf
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-09-27 13:32:07 -0500

    EJS for posts index

--- migrations/20160922105445_make_test.js
+++ migrations/20160922105445_make_test.js
@@ -4,11 +4,15 @@ exports.up = function(knex, Promise) {
 // name ( var char, limit 25, constraint UNIQUE NOT NULL)
 
 
-  return knex.schema.createTable("users", function(table){
-  	table.string("username").primary().unique().notNullable();
-  });
+return knex.schema.createTableIfNotExists("posts", function(table){
+     table.increments().primary();
+     table.string("title", 100).notNullable();
+     table.string("content_link").notNullable();
+     table.string('username');
+     table.foreign('username').references('users.username')
+});
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+return knex.schema.dropTableIfExists('posts');
 };
--- server.js
+++ server.js
@@ -5,13 +5,32 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var knex = require('./db/knex');
 var ejs = require('ejs');
+var methodOverride = require('method-override')
+
 
 app.set("view engine", "ejs"); //by default is looking for a folder called views
 // app.use(express.static(__dirname + "/public"));
 app.use(morgan("tiny"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 
 //USE METHOD OVERRIDE TO DELETE OR SEND AJAX REQUEST TO DELETE
+
+////////////////////////////////////////////////////////////////////////////////////
+//posts is the homepage, so / also goes to /posts
+app.get('/', function(req,res){
+    knex('posts').then(function(data){
+    res.render('posts/postsindex', {myPosts:data})
+    })
+})
 
 //the line below says if user sends a get request to this path, do this thing.
 app.get('/users/new', function(req, res) {
@@ -36,18 +55,50 @@ app.post('/users', function(req, res) {
         res.redirect('/users')
     })
 });
-
+////////////////////////////////////////////////////////////////////////////////////
+//display all posts
 app.get('/posts', function(req,res){
     knex('posts').then(function(data){
     res.render('posts/postsindex', {myPosts:data})
     })
 })
-//posts is the homepage, so / also goes to /posts
-app.get('/', function(req,res){
+
+    // 'displays all posts
+app.get('/posts/new', function(req, res) {
+    res.render('posts/postsnew')
+})
+
+//post to posts column in database from form on posts/new page
+app.post('/posts', function(req,res){
+    var wholePost = req.body;
+    var username = req.body.username;
+    console.log(username)
+    var arrayOfUsers = [];
     knex('posts').then(function(data){
-    res.render('posts/postsindex', {myPosts:data})
+        console.log(data)
+    data.forEach(function(item){
+        arrayOfUsers.push(item.username)
+    })
+    if (arrayOfUsers.indexOf(username)== -1){
+        res.render('posts/postsindex', {myError: "This user does not exist."})
+    }
+    else{
+        console.log(wholePost.content_link)
+        if(wholePost.content_link.indexOf("http")==-1){
+            wholePost.content_link = "http://" + wholePost.content_link;
+            if(wholePost.content_link.indexOf(".com")==-1){
+                    wholePost.content_link =  wholePost.content_link + ".com";
+                }
+        }
+    knex('posts').insert(wholePost).then(function(err){
+            res.redirect('/posts')
+            })
+         }
     })
 })
+
+
+
 
 //display a specific post
 app.get('/posts/:id', function(req,res){
@@ -58,7 +109,6 @@ app.get('/posts/:id', function(req,res){
     // res.send(data)
     })
 })
-
 //display all comments for a specific post
 app.get('/posts/:id/comments', function(req,res){
     var postTitle = req.params.id
@@ -67,6 +117,8 @@ app.get('/posts/:id/comments', function(req,res){
 })
 })
 
+
+////////////////////////////////////////////////////////////////////////////////////
 //** displays form to comment on a specified post
 app.get('/comments/new', function(req,res){
     var postTitle = req.body;
@@ -78,18 +130,8 @@ app.get('/comments/new', function(req,res){
     })
 })
 
-//post to posts column in database from form on posts/new page
-app.post('/posts', function(req,res){
-    var postTitle = req.body;
-    knex('posts').insert(postTitle).then(function(err){
-        res.redirect('/posts')
-    })
-})
 
-app.get('/posts/new', function(req, res) {
-    // 'displays all posts'
-    res.render('posts/postsnew')
-})
+
 
 
 
