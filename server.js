8d8460175cbceb242bd46f1e37bab7e1c91b6e5f
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-02 17:11:47 -0500

    adds delete functionality for comments

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var knex = require('./db/knex');
 var ejs = require('ejs');
 var methodOverride = require('method-override')
 
-
 app.set("view engine", "ejs"); //by default is looking for /views/
 //  one single / before path means "start from server root"
 // app.use(express.static(__dirname + "/public"));
@@ -29,15 +28,36 @@ app.get('/users/new', function(req, res) {
     res.render('users/usersnew');
 })
 
+// READ/see all users
 app.get('/users', function(req,res){
     //displays all users
     knex('users').then(function(data){
         var myUsers = JSON.stringify(data)
-        // console.log(data, "********these are the users from GET /users route")
         res.render('users/usersindex', {username: data})
     })
 })
 
+// CREATE a user. posts new user to users table on database
+app.post('/users', function(req, res) {
+    var newUsersName = req.body;  // (make sure you have body parser)
+    var arrayOfUsers = [];
+    knex('users').then(function(data){
+        console.log(data)
+        data.forEach(function(item){
+            arrayOfUsers.push(item.username) })
+            console.log(arrayOfUsers)
+            var newUser = newUsersName.username
+            if (arrayOfUsers.indexOf(newUser)){
+            knex('users').insert(newUsersName).then(function(err){
+                res.redirect('/users')
+            })
+        }else{
+            res.send("Error: This user already exists. Please go back and create a unique username.")
+        }
+    })
+});
+
+//DELETE a user
 app.delete('/users/:username', function(req,res) {
     var userToDelete = req.params.username;
     knex('comments').where('by_username', userToDelete).del()
@@ -50,39 +70,14 @@ app.delete('/users/:username', function(req,res) {
     )
 });
 
-app.delete('/posts/:id', function(req,res) {
-    var postToDelete = req.params.id;
-    knex('comments').where('post_id', postToDelete).del()
-        .then(knex('posts').where('id', postToDelete).del()
-        .then(function(){
-            res.redirect('/posts')
-        })
-        )
-});
-
-app.delete('posts/:id/comments/:id', function(){
 
 
-})
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////
-//display all posts
-app.get('/posts', function(req,res){
-    knex('posts').then(function(data){
-        // console.log(data);
-    res.render('posts/postsindex', {myPosts:data})
-    })
-})
-
+//CREATE new posts
 app.get('/posts/new', function(req, res) {
     res.render('posts/postsnew')
 })
 
-//posts to posts table in db from form on posts/new page
+//will post to posts table in db from form on posts/new page
 app.post('/posts', function(req,res){
     var wholePost = req.body;
     var username = req.body.username;
@@ -108,27 +103,15 @@ app.post('/posts', function(req,res){
     })
 })
 
-// posts new user to users table on database
-app.post('/users', function(req, res) {
-    var newUsersName = req.body;  // (make sure you have body parser)
-    var arrayOfUsers = [];
-    knex('users').then(function(data){
-        console.log(data)
-        data.forEach(function(item){
-            arrayOfUsers.push(item.username) })
-            console.log(arrayOfUsers)
-            var newUser = newUsersName.username
-            if (arrayOfUsers.indexOf(newUser)){
-            knex('users').insert(newUsersName).then(function(err){
-                res.redirect('/users')
-            })
-        }else{
-            res.send("Error: This user already exists. Please go back and create a unique username.")
-        }
+//READ/display all posts
+app.get('/posts', function(req,res){
+    knex('posts').then(function(data){
+        // console.log(data);
+    res.render('posts/postsindex', {myPosts:data})
     })
-});
+})
 
-//display a specific post
+//READ/display a specific post
 app.get('/posts/:id', function(req,res){
     var postTitle = req.params.id
     knex('posts').where('id','=', postTitle).then(function(data){
@@ -138,48 +121,29 @@ app.get('/posts/:id', function(req,res){
     })
 })
 
+//DELETE posts
+app.delete('/posts/:id', function(req,res) {
+    var postToDelete = req.params.id;
+    knex('comments').where('post_id', postToDelete).del()
+        .then(knex('posts').where('id', postToDelete).del()
+        .then(function(){
+            res.redirect('/posts')
+        })
+        )
+});
 
 
 
 
 
-//
-// knex.from('users').innerJoin('accounts', 'users.id', 'accounts.user_id')
-// Outputs:
-// select * from "users" inner join "accounts" on "users"."id" = "accounts"."user_id"
-// knex.table('users').innerJoin('accounts', 'users.id', '=', 'accounts.user_id')
-// Outputs:
-// select * from "users" inner join "accounts" on "users"."id" = "accounts"."user_id"
-// knex('users').innerJoin('accounts', function() {
-//   this.on('accounts.id', '=', 'users.account_id').orOn('accounts.owner_id', '=', 'users.id')
-// })
-// Outputs:
-// select * from "users" inner join "accounts" on "accounts"."id" = "users"."account_id" or "accounts"."owner_id" = "users"."id"
 
 
 
-//display all comments for a specific post
-app.get('/posts/:id/comments', function(req,res){
-    var postId = req.params.id;
-    knex('posts').innerJoin('comments', 'posts.id', 'comments.post_id').then(function(data){
-        // var myComments = JSON.stringify(data);
-        var specificPostComments = [];
-        for (var i = 0; i < data.length; i++){
-            if(data[i].post_id == postId){
-                specificPostComments.push(data[i])
-            }
-        }
-        // specificPostComments = JSON.stringify(specificPostComments)
-        console.log(specificPostComments, "all comments matching")
-
-    res.render('comments/commentsedit', {myPostComments: specificPostComments})
-// res.send(specificPostComments)
-})
-})
 
 
-////////////////////////////////////////////////////////////////////////////////////
-// displays a specific post and form to comment on that post
+
+
+// CREATE comments; displays a specific post and form to comment on that post
 app.get('/posts/:id/comments/new', function(req,res){
     var postId = req.params.id;
     console.log(postId, "***postTitle")
@@ -202,9 +166,33 @@ app.post('/posts/:id', function(req, res){
 })
 
 
+//READ all comments for a specific post
+app.get('/posts/:id/comments', function(req,res){
+    var postId = req.params.id;
+    knex('posts').innerJoin('comments', 'posts.id', 'comments.post_id').then(function(data){
+        // var myComments = JSON.stringify(data);
+        var specificPostComments = [];
+        for (var i = 0; i < data.length; i++){
+            if(data[i].post_id == postId){
+                specificPostComments.push(data[i])
+            }
+        }
+        console.log(specificPostComments, "all comments matching")
+    res.render('comments/commentsedit', {myPostComments: specificPostComments})
+// res.send(specificPostComments)
+})
+})
 
-
-
+//DELETE a comment on a specific post
+app.delete('/posts/:id/comments/:iD', function(req,res){
+    var postId = req.params.id
+    var commentId = req.params.iD;
+    knex('comments').where({id: commentId, post_id: postId}).del()
+    .then(function(){
+        res.redirect('/posts/'+postId+'/comments')
+    })
+    // res.send("comments delete is working")
+})
 
 
 
